c54f7cd9aed79535495770ea98b97a08829eea6b
GianluGiarde <gianlugiarde@gmail.com>
2023-05-17 16:38:35 +0200

    Add AppHero component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,8 @@
+<script></script>
+
+<template>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat deserunt aliquid nobis est corporis ipsum deleniti
+        cum eveniet minus voluptatum a ratione sit dolorum, debitis similique eum, recusandae, praesentium officiis?</p>
+</template>
+
+<style scoped></style>
